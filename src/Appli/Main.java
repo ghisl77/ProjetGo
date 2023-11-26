c11a13598ee384c920fdd2b5d291d3b3fbcf2167
Vegaliite <129175206+Vegaliite@users.noreply.github.com>
2023-11-26 12:19:54 +0100

    Lecture du parametre "taille du goban"

--- src/Appli/Main.java
+++ src/Appli/Main.java
@@ -1,9 +1,19 @@
 package Appli;
 
+import Game.*;
+
 public class Main {
     public static void main(String[] args) {
         
+    	boolean game_is_running = true;
     	
+    	GoGame partie = new GoGame(args[1]==null ? 19 : Integer.parseInt(args[1]));
+    	
+    	while(game_is_running) {
+    		
+    		
+    		
+    	}
     	
     }
 }
