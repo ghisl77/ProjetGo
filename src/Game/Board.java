6f409765af2f5e06a8d1571220d0e1c5c4adead1
ghisl77 <129007773+ghisl77@users.noreply.github.com>
2023-11-26 13:38:36 +0100

    gogame avec tableau de point et board avec le tableau

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -2,29 +2,36 @@ package Game;
 
 public class Board {
     private int nbCase ;
-    final static char[] alphabet = {'A','B','C','D','E','F','G','H','J','K','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    final static char[] alphabet = {'A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    private char[][] tableau;
     
     public Board(int nb){
-        nb=nbCase;
+        nbCase=nb;
+        tableau = new char[nb][nb];
+        for (int i = 0; i < nb; i++) {
+            for (int j = 0; j < nb; j++) {
+                tableau[i][j] = '.';
+            }
+        }
     }
     
     public String showboard(){
         StringBuilder s = new StringBuilder();
         s.append("  ");
-        for (int i = 0; i < nbCase+1; i++) {
+        for (int i = 0; i < nbCase; i++) {
             if(i!=0){s.append(i + " ");}
-            for (int j = 0; j < nbCase+1; j++) {
+            for (int j = 0; j < nbCase; j++) {
                 if(i==0){
                     s.append(alphabet[j]+ " ");
                 }
                 else
-                    s.append(". ");
+                    s.append(tableau[i][j]);
             }
             if(i!=0){s.append(i + " ");}
             s.append("\n");
         }
         s.append("  ");
-        for (int i = 0; i < nbCase+1; i++) {
+        for (int i = 0; i < nbCase; i++) {
             s.append(alphabet[i]+ " ");
         }
         return s.toString();
--- src/Game/GoGame.java
+++ src/Game/GoGame.java
@@ -3,11 +3,15 @@ package Game;
 public class GoGame {
     private Board goban;
     private IPlayer[] joueurs;
-    
+
+    private int[] points;
     public GoGame(int nbCase){
         goban = new Board(nbCase);
         joueurs = new IPlayer[2];
+        points = new int[2];
+        System.out.println(goban.showboard());
     }
-    
-    
+    public GoGame(){
+        this(19);
+    }
 }
