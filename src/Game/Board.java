113b9a0ed29a6fd6550fc82ab4d68c7daabac37e
Vegaliite <129175206+Vegaliite@users.noreply.github.com>
2023-12-03 14:39:48 +0100

    Update Board.java
    Ajout du tableau alphabet

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -5,8 +5,8 @@ package Game;
 import java.util.HashMap;
 
 public class Board {
+    private final char[] alphabet = {'A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     private final int nbCase;
-
     private char[][] tableau;
 
 
