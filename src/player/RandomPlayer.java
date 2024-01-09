020279a7a09a92534b943e23a7715d65a6779697
ghisl77 <129007773+ghisl77@users.noreply.github.com>
2024-01-09 11:15:29 +0100

    Update RandomPlayer.java

--- src/player/RandomPlayer.java
+++ src/player/RandomPlayer.java
@@ -1,13 +1,35 @@
 package player;
 
+import Game.Board;
 import Game.IPlayer;
+import Game.Intersection;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomPlayer implements IPlayer {
 
+	private Random rng;
+	private Board goban;
+	private String color;
+	
+	public RandomPlayer(Board goban,char stone) {
+		this.rng = new Random();
+		this.goban = goban;
+		this.color = stone=='b'?"black ":"white ";
+	}
+	
     @Override
     public String getCommand() {
-        return "play black a1";
+    	
+    	ArrayList<Intersection> potentialMoves = goban.getPlayableIntersections();
+    	
+    	if (potentialMoves.isEmpty()) {
+    		return "play";
+    	}
+    	
+    	Intersection move = potentialMoves.get(rng.nextInt(potentialMoves.size()));
+    	
+        return "play " + color + move.toString() ;
     }
 }
